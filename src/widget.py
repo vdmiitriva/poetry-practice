7eee98d78bfe59a799acb4ba71d7f4b7a32b2efb
Viktoriia <v.dmiitriva@gmail.com>
2026-09-14 14:13:06 +0300

    add date formatting function

--- src/widget.py
+++ src/widget.py
@@ -1,18 +1,20 @@
 from src.masks import get_mask_account, get_mask_card_number
 from datetime import datetime
 
+
 def mask_account_card(s: str) -> str:
     parts = s.split()
-    if len(parts) < 2: return ""
+    if len(parts) < 2:
+        return ""
 
-    name = ' '.join(parts[:-1])
+    name = " ".join(parts[:-1])
     number = parts[-1]
 
-    if name == 'Счет':
+    if name == "Счет":
         return f"{name} {get_mask_account(number)}"
     return f"{name} {get_mask_card_number(number)}"
+
 
 def get_date(s: str) -> str:
     date = datetime.fromisoformat(s)
     return date.strftime("%d.%m.%Y")
-
